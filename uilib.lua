819b25f39cf20f2ec86c63c177f410fcb73c9109
x2Eterniz <96688583+x2Eterniz@users.noreply.github.com>
2026-05-30 21:04:48 +0700

    Update uilib.lua

--- uilib.lua
+++ uilib.lua
@@ -384,7 +384,7 @@ function DarkUI:CreateWindow(config)
 
 	local headerHeight = 52
 	local tabHeight = 48
-	local tabWidth = config.TabWidth or 158
+	local tabWidth = config.TabWidth or 182
 	local tabContentGap = 10
 	local contentTopOffset = config.ContentTopOffset or 20
 	local searchHeight = config.Search == false and 0 or 42
